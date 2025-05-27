eff475ddfbddff1cf8ca27ffa59518c9e765e813
Vishal Yadav <vy372984@gmail.com>
2025-05-27 18:37:02 +0530

    The failed to fetch error

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.getElementById('generate-btn').addEventListener('click', function() {
     const data = document.getElementById('data').value;
     if (data) {
-        fetch('http://127.0.0.1:5000/generate_qr', {
+        fetch('/generate_qr', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
